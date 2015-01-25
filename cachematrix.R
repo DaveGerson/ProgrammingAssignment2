64833a49f26bf80f9538087ae8e53703e6a19345
Gersonda <GersonDave@gmail.com>
2015-01-24 22:12:07 -0600

    Added My code to complete assignment
    My code has been completed and tested.  I also added a computeNow
    feature.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My first function creates a list with the matrix object embedded in it.
+## This matrix object has getters and setters which enables caches.
 
-## Write a short comment describing this function
+## You interact with it via the getter setter API's.  A slight change is that there is a flag which, if called, will populate the 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(), computeNow = 0) {
+        require(MASS)
+		
+		inv <- NULL
+		if(computeNow == 1) {
+                message("creating inverse now")
+                inv <- solve(x)
+        }	
+		
+        set <- function(y,computeNow = 1) {
+                x <<- y
+                m <<- NULL
+				if(computeNow == 1) {
+						message("creating inverse now")
+						inv <<- solve(x)
+				}	
+				
+        }
+		
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        #Output
+		list(
+			set = set,
+			get = get,
+            setinverse = setinverse,
+            getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## If the computeNow option is not called in the initial setup of the cachematrix object
+## The inverse is then calculated in this function and set in the original object
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+		inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinverse(inv)
+        inv
 }
